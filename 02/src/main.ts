00af0b6db75380321d9da1998dca6e6d25376e80
klmagpan <klmagpan@ucsc.edu>
2025-08-05 23:43:31 -0700

    Remove files from previously named  directory

--- 02/src/main.ts
+++
@@ -1,9 +0,0 @@
-const div = document.querySelector("#app") // Get an element from CSS selector
-
-console.log(div)
-
-const p = document.createElement("p"); // creates paragraph elemet in body
-
-p.textContent = "Welcome to our coffee shop!";
-
-div.append(p);
